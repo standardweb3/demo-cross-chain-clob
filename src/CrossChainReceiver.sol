00acd08faee3dbd1a8b2a9ca5c791334d9da2279
Hyungsuk Kang <hskang9@gmail.com>
2025-04-05 14:15:06 +0700

    build receiver

--- src/CrossChainReceiver.sol
+++ src/CrossChainReceiver.sol
@@ -7,10 +7,13 @@ import "@standardweb3/exchange/interfaces/IMatchingEngine.sol";
 
 contract CrossChainReceiver is TokenReceiver {
     // The wormhole relayer and registeredSenders are inherited from the Base.sol contract.
+    address public matchingEngine;
 
-    constructor(address _wormholeRelayer, address _tokenBridge, address _wormhole)
+    constructor(address _wormholeRelayer, address _tokenBridge, address _wormhole, address _matchingEngine)
         TokenBase(_wormholeRelayer, _tokenBridge, _wormhole)
-    {}
+    {
+        matchingEngine = _matchingEngine;
+    }
 
     // Function to receive the cross-chain payload and tokens with emitter validation
     function receivePayloadAndTokens(
@@ -27,5 +30,19 @@ contract CrossChainReceiver is TokenReceiver {
 
         // Transfer the received tokens to the intended recipient
         IERC20(receivedTokens[0].tokenAddress).transfer(recipient, receivedTokens[0].amount);
+    }
+
+    function receiveCrossChainOrder(
+        bytes memory payload,
+        TokenReceived[] memory receivedTokens,
+        bytes32 sourceAddress,
+        uint16 sourceChain,
+        bytes32 // deliveryHash (not used in this implementation)
+    ) internal override onlyWormholeRelayer isRegisteredSender(sourceChain, sourceAddress) {
+        require(receivedTokens.length == 1, "Expected 1 token transfer");
+
+        // call the matching engine to submit the order
+        (bool success, bytes memory data) = matchingEngine.call(payload);
+        require(success, "MatchingEngine call failed");
     }
 }
